e8f322f58088954c97437cec719afb472e76ef7b
Ezequiel Ceruti <92703582+Zequi009@users.noreply.github.com>
2024-05-01 14:30:37 -0300

    v0.02

---
+++ manada.js
@@ -0,0 +1,43 @@
+const image = document.getElementById('image');
+
+image.addEventListener('mouseenter', function() {
+  image.classList.add('hover');
+  image.src = 'imagenes/NuevaCele3.png'; // Cambia a la segunda imagen
+});
+
+image.addEventListener('mouseleave', function() {
+  image.classList.remove('hover');
+  image.src = 'imagenes/baloo3.png'; // Vuelve a la primera imagen
+});
+
+const image2 = document.getElementById('image2');
+
+image2.addEventListener('mouseenter', function() {
+  image2.classList.add('hover');
+  image2.src = 'imagenes/NuevaMica3.png'; // Cambia a la segunda imagen
+});
+
+image2.addEventListener('mouseleave', function() {
+  image2.classList.remove('hover');
+  image2.src = 'imagenes/bagheera.png'; // Vuelve a la primera imagen
+});
+
+
+//Verificar Contraseña
+function VerificarContraseña() {
+  var contraseñaInput = document.getElementById("contraseña");
+  var contraseñaIngresada = contraseñaInput.value;
+  var mensajeError = document.getElementById('mensajeError')
+  if (contraseñaIngresada === "manada1234") {
+      window.location.href = "Index.html";
+  } else {
+      contraseñaInput.classList.add("is-invalid");
+      mensajeError.classList.remove("d-none");
+
+  }
+}
+
+document.getElementById("olvContraseña").addEventListener("click", function() {
+  // Mostrar mensaje
+  alert("¡Has hecho clic en el botón!");
+});
